dc1c67d866525d4b7a4163ba7361b00261cc0580
я <netsend321@gmail.com>
2022-07-20 10:09:27 +0500

    2

--- src/main/java/my/bot/App.java
+++ src/main/java/my/bot/App.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 
